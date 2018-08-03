f707b074c03d877e1c111a12ac639f1ac0146066
Gareth Field <field.gareth@gmail.com>
2018-08-02 20:37:08 -0400

    Video 5 of 8 5:00 walls and pickup, really. Just got rotation

---
+++ Scripts/Rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	// Update is called once per frame
+	void Update () {
+		// Rotate and Translate, both options :)
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+		// This action also needs to be smooth and framerate independent,
+		// hence multiplication by Time.deltaTime.
+	}
+}
