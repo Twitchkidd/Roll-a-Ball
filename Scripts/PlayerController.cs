44adeb381eeb041f966906ba2be01347afc98670
Gareth Field <field.gareth@gmail.com>
2018-08-03 15:39:29 -0400

    Video 7 of 8 end:

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,20 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
   public float speed;
+  public Text countText;
+  public Text winText;
   private Rigidbody rb;
+  private int count;
+  void SetCountText()
+  {
+    countText.text = "Count: " + count.ToString();
+    if (count >= 12)
+    {
+      winText.text = "You win!";
+    }
+  }
   void Start()
   {
     rb = GetComponent<Rigidbody>();
+    count = 0;
+    SetCountText();
+    winText.text = "";
   }
   void OnTriggerEnter(Collider other)
   {
     if (other.gameObject.CompareTag("PickUp"))
     {
       other.gameObject.SetActive(false);
+      count++;
+      SetCountText();
     }
   }
   void FixedUpdate()
